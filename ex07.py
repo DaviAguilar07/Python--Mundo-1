83e99807563e8baef62fd0aabffe6b4227fc18be
Davi Aguilar <aguilardavi44@gmail.com>
2026-08-28 17:11:45 -0300

    Atividade 7

---
+++ ex07.py
@@ -0,0 +1,3 @@
+nota1 = int(input('Insira a sua primeira nota: '))
+nota2 = int(input('Insira a sua segunda nota: '))
+print('A média dos valores {} e {}, é {}'.format(nota1, nota2, (nota1 + nota2) / 2))
