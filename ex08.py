2fd1ca25970966d05123afd0bb5f3da5b97169a6
Davi Aguilar <aguilardavi44@gmail.com>
2026-08-28 17:12:31 -0300

    Atividade 8

---
+++ ex08.py
@@ -0,0 +1,2 @@
+metros = float(input('Informe a quantidade de metros da parede do seu quarto: '))
+print('O valor é {:.2f}\nEm centímetros são {:.2f}\nEm milimetros são {:.2f}'.format(metros, metros * 100, metros * 1000))
