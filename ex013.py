28883994e5ea89c1f3baddc9a606f41f9e67fe69
Davi Aguilar <aguilardavi44@gmail.com>
2026-08-29 16:19:48 -0300

    Atividade 13

---
+++ ex013.py
@@ -0,0 +1,4 @@
+salario = float(input('Informe seu salário: R$ '))
+aumento = salario + (salario * 15/100)
+print('O seu salário é R${}, após o aumento de 15%, fica R${}'.format(salario, aumento))
+
